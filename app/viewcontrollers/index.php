94794d93448c17869428327439315606e02e6da2
Artur Z <doslunar8@gmail.com>
2024-01-21 17:19:42 +0100

    Move db creation to onstart

--- app/viewcontrollers/index.php
+++ app/viewcontrollers/index.php
@@ -1,32 +1,6 @@
 <?php
 namespace App;
 
-// Move this to onstart.php or app.php later
-$posts_sql = `
-CREATE TABLE IF NOT EXISTS blog_posts (
-  blogpostid INTEGER PRIMARY KEY AUTOINCREMENT,
-  author INTEGER,
-  coverimage TEXT,
-  title TEXT,
-  content TEXT,
-  visibility TEXT DEFAULT 'private',
-  posted_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (author) REFERENCES user(id)
-);
-`;
-
-$users_sql = `
-CREATE TABLE IF NOT EXISTS user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL UNIQUE,
-  name TEXT NOT NULL,
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
-);`;
-$conn = sql_connect("db.sql");
-sql_query($conn, $posts_sql);
-sql_query($conn, $users_sql);
-
 require_once('app/common.php');
 session_start();
 
